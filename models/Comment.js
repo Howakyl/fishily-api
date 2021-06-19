7ce2eb7d386563e76b7f919be814084841625d81
Howakyl <howakyl@gmail.com>
2021-06-19 13:35:50 -0700

    add Comment model

---
+++ models/Comment.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema;
+
+const CommentSchema = new Schema({
+  description: {
+    type: String,
+    maxlength: 300,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now()
+  },
+  user: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User'
+  },
+  post: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Post'
+  }
+}, {timestamps: true});
+
+const Comment = mongoose.model("Comment", CommentSchema);
+
+module.exports = Comment;
