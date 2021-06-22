92db99bc4cc9e16e1e246dec76b8cb0a7ba907ec
Howakyl <howakyl@gmail.com>
2021-06-22 13:00:02 -0700

    delete comment route

--- routes/comments.js
+++ routes/comments.js
@@ -4,5 +4,6 @@ const ctrl = require('../controllers');
 // /api/fishily/comments
 router.get("/", ctrl.comments.index);
 router.post("/:id", ctrl.comments.create);
+router.delete('/:id', ctrl.comments.destroy);
 
 module.exports = router;
