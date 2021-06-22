23c357273012a4577a9329dab2df3c4172f362c1
Howakyl <howakyl@gmail.com>
2021-06-22 14:37:06 -0700

    update comment route

--- routes/comments.js
+++ routes/comments.js
@@ -5,6 +5,7 @@ const ctrl = require('../controllers');
 router.get("/", ctrl.comments.index);
 router.get("/:id", ctrl.comments.show);
 router.post("/:id", ctrl.comments.create);
+router.put(":/id", ctrl.comments.update);
 router.delete('/:id', ctrl.comments.destroy);
 
 module.exports = router;
