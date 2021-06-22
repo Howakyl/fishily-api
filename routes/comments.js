80fc67bb52f2a04b7839c497398964e238261c3e
Howakyl <howakyl@gmail.com>
2021-06-22 13:21:35 -0700

    show comment route

--- routes/comments.js
+++ routes/comments.js
@@ -3,6 +3,7 @@ const ctrl = require('../controllers');
 
 // /api/fishily/comments
 router.get("/", ctrl.comments.index);
+router.get("/:id", ctrl.comments.show);
 router.post("/:id", ctrl.comments.create);
 router.delete('/:id', ctrl.comments.destroy);
 
