95d4e296640f2286a7bae2156c219287853a13e2
Howakyl <howakyl@gmail.com>
2021-06-23 13:15:16 -0700

    format files to 2 spaces tab size

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,120 +1,110 @@
 const db = require("../models/");
 
 // ALL POSTS
-const index = (req,res) => {
-    db.Post.find({})
-        .populate("user")
-        .then((foundPosts) => {
-            res.json({ posts: foundPosts});
-        })
-        .catch((err) => {
-            console.log('error: ', err);
-            res.json({ Error: 'Unable to retrieve post data.'});
-        })
+const index = (req, res) => {
+  db.Post.find({})
+    .populate("user")
+    .then((foundPosts) => {
+      res.json({ posts: foundPosts });
+    })
+    .catch((err) => {
+      console.log("error: ", err);
+      res.json({ Error: "Unable to retrieve post data." });
+    });
 };
 
 // SHOW POST
-const show = (req,res) => {
-
-    db.Post.findById(req.params.id)
-        .populate("user")
-        .populate("comments")
-        .then((foundPost) => {
-            res.json({ post: foundPost });
-        })
-        .catch((err) => {
-            console.log('error fetching post data', err);
-            res.json({ Error: 'Unable to fetch post data'});
-        });
+const show = (req, res) => {
+  db.Post.findById(req.params.id)
+    .populate("user")
+    .populate("comments")
+    .then((foundPost) => {
+      res.json({ post: foundPost });
+    })
+    .catch((err) => {
+      console.log("error fetching post data", err);
+      res.json({ Error: "Unable to fetch post data" });
+    });
 };
 
 // ADD POSTS
-const create = (req,res) => {
-
-    const userId = req.params.id;
-    db.User.findById(userId)
-        .then((foundUser) => {
-            req.body.user = userId;
-            db.Post.create(req.body)
-                .then((createdPost) => {
-                    foundUser.posts.push(createdPost._id);
-                    foundUser.save((err, savedUser) => {
-                        if(err) return console.log(err);
-                    });
-                    res.json({ post: createdPost});
-                })
-                .catch((err) => {
-                    console.log('error creating post: ', err);
-                    res.json({ Error: 'Unable to create post.'});
-                })
-            
+const create = (req, res) => {
+  const userId = req.params.id;
+  db.User.findById(userId)
+    .then((foundUser) => {
+      req.body.user = userId;
+      db.Post.create(req.body)
+        .then((createdPost) => {
+          foundUser.posts.push(createdPost._id);
+          foundUser.save((err, savedUser) => {
+            if (err) return console.log(err);
+          });
+          res.json({ post: createdPost });
         })
         .catch((err) => {
-            console.log('error finding user: ', err);
-            res.json({ Error: 'Unable to find user.'});
+          console.log("error creating post: ", err);
+          res.json({ Error: "Unable to create post." });
         });
+    })
+    .catch((err) => {
+      console.log("error finding user: ", err);
+      res.json({ Error: "Unable to find user." });
+    });
 };
 
 //UPDATE POST
-const update = (req,res) => {
-
-    db.Post.findByIdAndUpdate(
-        req.params.id,
-        req.body,
-        { new: true})
-            .then((updatedPost) => {
-                res.json({ post: updatedPost });
-            })
-            .catch((err) => {
-                console.log('error updating post: ', err);
-                res.json({ Error: 'Unable to update post. '});
-            });
+const update = (req, res) => {
+  db.Post.findByIdAndUpdate(req.params.id, req.body, { new: true })
+    .then((updatedPost) => {
+      res.json({ post: updatedPost });
+    })
+    .catch((err) => {
+      console.log("error updating post: ", err);
+      res.json({ Error: "Unable to update post. " });
+    });
 };
 
 //DELETE POST, DELETES POST ON USER
-const destroy = (req,res) => {
-    const postId = req.params.id;
+const destroy = (req, res) => {
+  const postId = req.params.id;
 
-    db.Post.findByIdAndDelete(postId)
-        .then((deletedPost) => {
+  db.Post.findByIdAndDelete(postId)
+    .then((deletedPost) => {
+      db.User.findOne({ posts: postId }, (err, foundUser) => {
+        if (err) return console.log(err);
 
-            db.User.findOne({'posts': postId} , (err, foundUser) => {
-                if (err) return console.log(err);
-
-                foundUser.posts.remove(postId);
-                foundUser.save((err, savedUser) => {
-                    if (err) return console.log(err);
-                });
-            });
-
-            res.json({ post: deletedPost });
-        })
-        .catch((err) => {
-            console.log('error deleting post: ', err);
-            res.json({ Error: 'unable to delete post.'});
+        foundUser.posts.remove(postId);
+        foundUser.save((err, savedUser) => {
+          if (err) return console.log(err);
         });
+      });
+
+      res.json({ post: deletedPost });
+    })
+    .catch((err) => {
+      console.log("error deleting post: ", err);
+      res.json({ Error: "unable to delete post." });
+    });
 };
 
 // ALL POST COMMENTS
 const comments = (req, res) => {
-    db.Post.findById(req.params.id)
+  db.Post.findById(req.params.id)
     .populate("comments")
     .then((foundPost) => {
-        res.json({ comments: foundPost.comments })
+      res.json({ comments: foundPost.comments });
     })
     .catch((err) => {
-        if (err) console.log(err);
-        res.json({ Error: 'Unable to fetch comments'})
-    })
-
-
-}
+      if (err) console.log(err);
+      res.json({ Error: "Unable to fetch comments" });
+    });
+};
 
 module.exports = {
-    index,
-    create,
-    show,
-    update,
-    destroy,
-    comments
-}
+  index,
+  create,
+  show,
+  update,
+  destroy,
+  comments,
+};
