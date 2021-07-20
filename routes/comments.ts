fd2e073a46318f35470e52f82794807a9a709d1e
Howakyl <howakyl@gmail.com>
2021-07-19 21:13:17 -0700

    update comments routes

--- routes/comments.ts
+++ routes/comments.ts
@@ -1,5 +1,5 @@
-const router = require('express').Router();
-const ctrl = require('../controllers');
+export const router = require('express').Router();
+import * as ctrl from '../controllers';
 
 // /api/fishily/comments
 router.get("/", ctrl.comments.index);
